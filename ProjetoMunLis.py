2b9250624dbaf32afe8997852ebe8d32045a1c8e
jpps2408 <jpps2408@users.noreply.github.com>
2020-04-09 18:52:08 +0100

    Revert "Trash Again"
    This reverts commit 020cee64239cda89e317b6441606f2c2a22ac274.

--- ProjetoMunLis.py
+++ ProjetoMunLis.py
@@ -4,50 +4,59 @@ from toolsgis import *
 from ostools import *
 from timeparsingtools import *
 from cmd import Cmd
+from MasterHandler_ import * 
 
 ######################################################
 ################## ##################
 ######################################################
 
 
-class MyPrompt(Cmd):
-    prompt = 'pb> '
-    intro = "Welcome! Type ? to list commands"
+#class MyPrompt(Cmd):
+#    prompt = 'pb> '
+#    intro = "Welcome! Type ? to list commands"
  
-    def do_exit(self, inp):
-        print("Bye")
-        return True
+#    def do_exit(self, inp):
+#        print("Bye")
+#        return True
     
-    def help_exit(self):
-        print('exit the application. Shorthand: x q Ctrl-D.')
+#    def help_exit(self):
+#        print('exit the application. Shorthand: x q Ctrl-D.')
  
-    def do_add(self, inp):
-        print("adding '{}'".format(inp))
+#    def do_add(self, inp):
+#        print("adding '{}'".format(inp))
  
-    def help_add(self):
-        print("Add a new entry to the system.")
+#    def help_add(self):
+#        print("Add a new entry to the system.")
  
-    def default(self, inp):
-        if inp == 'x' or inp == 'q':
-            return self.do_exit(inp)
+#    def default(self, inp):
+#        if inp == 'x' or inp == 'q':
+#            return self.do_exit(inp)
  
-        print("Default: {}".format(inp))
+#        print("Default: {}".format(inp))
  
-    do_EOF = do_exit
-    help_EOF = help_exit
+#    do_EOF = do_exit
+#    help_EOF = help_exit
  
 
-if __name__ == '__main__':
-    MyPrompt().cmdloop()
-circuit_folder = r"C:\Users\JoaoPedro\Desktop\ArcGISTestMaker\Circuit_01\I0104"
-circuit_object = CD.CircuitDir(circuit_folder)
-circuit_object.make_CircuitPolygon(200)
+#if __name__ == '__main__':
+#    MyPrompt().cmdloop()
+#circuit_folder = r"C:\Users\JoaoPedro\Desktop\ArcGISTestMaker\Circuit_01\I0104"
+#circuit_object = CD.CircuitDir(circuit_folder)
+#circuit_object(False)
 
 
-realizacao = r"C:\Users\JoaoPedro\Desktop\ArcGISTestMaker\Circuit_01\I0104\aliasCircuitVoyages\aliasToDo\I0104_V2520_17_01_2020"
-shift = ShiftDir(realizacao,circuit_object)
-shift.process_shift(20)
 
+
+#path = input("Path:")
+#AS = AncientStructural(r"C:\Users\JoaoPedro\Documents\BIGDirectory")
+#AS.replace_AllKmls()
+
+circuit_folder = r"C:\Users\JoaoPedro\Documents\BIGDirectory\CIRCUITS\I0103"
+circuit = CD.CircuitDir(circuit_folder)
+circuit(False)
+#realizacao = r"C:\Users\JoaoPedro\Documents\BIGDirectory\CIRCUITS\I0103\aliasCircuitVoyages\aliasDoNe\I0103_V2538_03_01_2020"
+#shift = ShiftDir(realizacao,CD.CircuitDir(r"C:\Users\JoaoPedro\Documents\BIGDirectory\CIRCUITS\I0103"))
+#shift.process_shift()
 ######################################################
 ################## ##################
 ######################################################
--- timeparsingtools.py
+++ timeparsingtools.py
@@ -1,6 +1,5 @@
 import datetime
 import time
-import functools
 from ostools import *
 
 
